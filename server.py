08ea91771871bb16622d261336751efe1a4b64cb
vova <vovaproscurin9@gmail.com>
2023-07-01 20:40:19 +0300

    first commit

--- server.py
+++ server.py
@@ -1,8 +1,28 @@
 import time
 import socket
 
-main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#Настраиваем сокет
+main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)#Отключение пакетирование
+main_socket.bind(("localhost", 15200))
+main_socket.setblocking(False)
+main_socket.listen(5)
+print("Сокет создался")
+players = []
+while True:
+    try:
+        new_socket, addr = main_socket.accept()#Принимаем входящие
+        print('Подключился', addr)
+        new_socket.setblocking(False)
+        players.append(new_socket)
+    except BlockingIOError:
+        pass
+    for x in players:
+        try:
+            data = x.recv(1024).decode()
+            print("Получил", data)
+        except:
+            pass
+
 
 
 
