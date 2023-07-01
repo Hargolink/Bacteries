c7ab229fe95d0948a1b99ac370b55523897e12f9
vova <vovaproscurin9@gmail.com>
2023-07-01 21:02:21 +0300

    Добавили в клиент import socket, добавили подключение к server через ip и порт, сделали бесконечный цикл и отправляем сообщение "Привет"

--- client.py
+++ client.py
@@ -3,12 +3,6 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Настраиваем сокет
 sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)  # Отключение пакетирование
 
-sock.connect(("localhost", 15200))#Подключаемся к server через ip и порт
+sock.connect(("localhost", 15200))  # Подключаемся к server через ip и порт
 while True:
-
-    sock.send("Привет".encode())#Мы отправляем команду и кодируем
-
-
-
-
-
+    sock.send("Привет".encode())  # Мы отправляем команду и кодируем
