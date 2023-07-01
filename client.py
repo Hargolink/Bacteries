b479f0088594650075fc79be7fd4de86d41521be
vova <vovaproscurin9@gmail.com>
2023-07-01 21:21:43 +0300

    Добавили в клиент import socket, добавили подключение к server через ip и порт, сделали бесконечный цикл и отправляем сообщение "Привет"

--- client.py
+++ client.py
@@ -1,8 +1,14 @@
 import socket
+import pygame
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Настраиваем сокет
 sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)  # Отключение пакетирование
 
 sock.connect(("localhost", 15200))  # Подключаемся к server через ip и порт
+pygame.init()
+WIDTH = 800
+HEIGHT = 600
+dis = pygame.display.set_mode((WIDTH, HEIGHT))# Задаём размеры окошка
+pygame.display.set_caption("Бактерии") # Задаём имя окошка
 while True:
     sock.send("Привет".encode())  # Мы отправляем команду и кодируем
